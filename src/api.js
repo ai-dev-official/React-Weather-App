f97f566c384aa6d64511183eb8ddf31469e16c33
Azubuike <X00191169@myTUDublin.ie>
2022-07-17 09:46:47 +0100

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+
+export const geoApiOptions = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5670401c68msh467aa30dbbb72aep156bfcjsn7f7e8014b596',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
+
+    export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+    export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+
+    export const WEATHER_API_KEY="3e300a36990aa0092c603deef4ae90bf"
+
+    
